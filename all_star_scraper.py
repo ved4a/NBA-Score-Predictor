db74063dd530f81e941a164caac2df6c25c9157c
Vedaa Anand <work.vedaa@gmail.com>
2024-11-15 16:40:51 +0530

    feat: get player awards

--- all_star_scraper.py
+++ all_star_scraper.py
@@ -13,3 +13,13 @@ def get_player_seasons(player_id):
         print(f"Error fetching stats for player ID: {player_id}: {e}.")
         return []
     
+def fetch_player_awards(player_id, retries=3, delay=5):
+    for attempt in range(retries):
+        try:
+            print(f"Fetching Awards for Player ID {player_id} (Attempt {attempt + 1})...")
+            awards = playerawards.PlayerAwards(player_id=player_id, timeout=60)
+            return awards.get_normalized_dict()
+        except Exception as e:
+            print(f"Error fetching awards for Player ID {player_id} (Attempt {attempt + 1}): {e}")
+            time.sleep(delay)
+    return {"error": f"Failed to fetch awards for Player ID {player_id} after {retries} retries."}
