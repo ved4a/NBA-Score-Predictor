791705347a97b1f97f3487d0beeb2ae6ff06283a
Vedaa Anand <work.vedaa@gmail.com>
2024-11-19 23:51:16 +0530

    fix: add year last played all-star game

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -6,12 +6,16 @@ output_file = "all_stars_cleaned.csv"
 data = pd.read_csv(input_file)
 
 player_column = 'Player'
+year_column = 'Year'
 
 data = data[data[player_column].str.lower() != "reserves"] # remove 'reserves' as player
 
-player_counts = data[player_column].value_counts().reset_index()
-player_counts.columns = ['Player', 'Appearances']
+player_stats = (
+    data.groupby(player_column)[year_column]
+    .agg(Appearances='count', YearLastPlayed='max')
+    .reset_index()
+)
 
-player_counts.to_csv(output_file, index=False)
+player_stats.to_csv(output_file, index=False)
 
 print(f"Unique All-Star players and their appearances saved to '{output_file}'")
