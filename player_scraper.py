d91b6d6c59733ea61ccf34e3c0c6cf54faa5ed14
Vedaa Anand <work.vedaa@gmail.com>
2024-11-22 18:08:21 +0530

    fix: remove unnecessary lib, fix player name

--- player_scraper.py
+++ player_scraper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
-import random
 from concurrent.futures import ThreadPoolExecutor
 
 players = pd.read_csv("all_stars_cleaned.csv")
