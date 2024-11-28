3f42f39a224ac596ff688e3254c23a916796db05
Vedaa Anand <work.vedaa@gmail.com>
2024-11-28 19:21:47 +0530

    feat: get avg player stats for all features

--- scripts/get_performance_extremes.py
+++ scripts/get_performance_extremes.py
@@ -4,9 +4,9 @@ import math
 df = pd.read_csv("player_stats.csv")
 
 stats_columns = [
-    "Points", "Field Goals", "FG Attempts", "FG%", "Three Pointers", 
-    "3P Attempts", "3P%", "Free Throws", "FT Attempts", "FT%", 
-    "ORB", "DRB", "TRB", "Assists", "Blocks", "Steals", 
+    "Points", "Field Goals", "FG Attempts", "Three Pointers", 
+    "3P Attempts", "Free Throws", "FT Attempts",
+    "ORB", "DRB", "Assists", "Blocks", "Steals", 
     "Turnovers", "Personal Fouls"
 ]
 
@@ -17,16 +17,13 @@ for player in df['Player'].unique():
     result = {"Player": player}
     
     for stat in stats_columns:
-        result[f"Highest {stat}"] = player_data[stat].max()
-        result[f"Lowest {stat}"] = player_data[stat].min()
-
         # skip NaN and apply floor function to get avg points
         stat_mean = player_data[stat].mean(skipna=True)
-        result[f"{stat} Avg"] = math.floor(stat_mean) if not pd.isna(stat_mean) else 0
+        result[f"Average {stat}"] = math.floor(stat_mean) if not pd.isna(stat_mean) else 0
     
     results.append(result)
 
 high_low_avg_stats = pd.DataFrame(results)
 
-high_low_avg_stats.to_csv("player_HLA_stats.csv", index=False)
-print("High, low, and average stats calculated and saved to 'player_HLA_stats.csv'")
+high_low_avg_stats.to_csv("avg_player_stats.csv", index=False)
+print("Average stats calculated and saved to 'avg_player_stats.csv'")
