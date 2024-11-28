cd5bc379b190f4d0d1d52ba100c979f5e3d35698
Vedaa Anand <work.vedaa@gmail.com>
2024-11-28 16:30:31 +0530

    feat: load and pre-process data

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ current_season_data = pd.read_csv("current_szn_data.csv")
 previous_season_data = pd.read_csv("prev_szn_data.csv")
 
 data = pd.concat([current_season_data, previous_season_data])
+
+X = data.drop(columns=["Points"])
+y = data["Points"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
