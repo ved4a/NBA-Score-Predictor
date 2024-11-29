26895eb5a1472dd703da3f38b09b28b9589550df
Vedaa Anand <work.vedaa@gmail.com>
2024-11-29 19:16:50 +0530

    fix: rm numpy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
